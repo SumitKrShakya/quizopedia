6b03789be0e405df7c773a40b26748700db8cab4
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-10-15 23:20:04 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = [{
+    type: "mcq",
+    que: "this is question 1",
+    opt: ["Options A", "Option B", "Option C", "Option D"],
+    ans: 1
+}, {
+    type: "mcq",
+    que: "this is question 2",
+    opt: ["Options A", "Option B", "Option C", "Option D"],
+    ans: 1
+}, {
+    type: "mcq",
+    que: "this is question 3",
+    opt: ["Options A", "Option B", "Option C", "Option D"],
+    ans: 1
+}];
